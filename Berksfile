cf0c457c5aa4339b8e9290b93c766d2dd65e05ac
Alex Knoll <alex@newmediadenver.com>
2014-03-25 09:41:34 -0600

    Initial commit

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+metadata
+
+cookbook 'php', '~> 1.2.6'
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "drupal-developer"
+maintainer       "Alex Knoll"
+maintainer_email "arknoll@gmail.com"
+license          "Apache 2.0"
+description      "Installs/configures developer friendly tools"
+version          "0.1.0"
+recipe           "drupal-developer::default", "Installs/configures something"
+
+depends 'php'
