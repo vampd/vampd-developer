12b9d342af54553a1c047985f6fed50e20b7cc36
Tim Whitney <tim.d.whitney@gmail.com>
2015-01-14 08:56:35 -0700

    ADDED: Initial add of user_groups recipe

---
+++ recipes/user_groups.rb
@@ -0,0 +1,8 @@
+# Place users in certain groups.
+node[:vampd_developer][:user_groups].each do |group_name, users|
+  group group_name do
+    action :modify
+    members users
+    append true
+  end
+end
